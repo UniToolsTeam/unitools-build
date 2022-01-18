18f725edb1db3c6f036db32db09e61d694d3958c
Stanislav Felinskyi <stanisalv.felinskyi@innovecs.com>
2022-01-18 09:24:26 +0200

    Intermediate commit

--- Editor/IO/Archive/CreateZipArchiveStep.cs
+++ Editor/IO/Archive/CreateZipArchiveStep.cs
@@ -1,6 +1,5 @@
 using System;
 using System.IO;
-using System.Reflection;
 using System.Threading.Tasks;
 using UniTools.Build;
 using UniTools.CLI;
@@ -9,16 +8,44 @@ using UnityEngine;
 namespace UniTools.IO
 {
     [CreateAssetMenu(
-        fileName = nameof(ZipStep),
-        menuName = nameof(UniTools) + "/" + nameof(IO) + "/Zip"
+        fileName = nameof(CreateZipArchiveStep),
+        menuName = nameof(UniTools) + "/" + nameof(IO) + "/" + nameof(CreateZipArchiveStep)
     )]
-    public sealed class ZipStep : ScriptablePostBuildStep
+    public sealed class CreateZipArchiveStep : ScriptablePostBuildStep
     {
         [SerializeField, Tooltip("The location of the folder that needs to be compressed.")] private PathProperty m_directory = default;
         [SerializeField, Tooltip("The name of the created archive (include extensions. Ex: my.zip).")] private string m_archiveFileName = default;
 
         public override async Task Execute(string pathToBuiltProject)
         {
+            if (string.IsNullOrWhiteSpace(m_archiveFileName))
+            {
+                throw new Exception($"{nameof(CreateZipArchiveStep)}: file name is null or empty!");
+            }
+
+            var ext = Path.GetExtension(m_archiveFileName);
+            Debug.LogError(ext);
+            if (!ext.Equals(".zip"))
+            {
+                throw new Exception($"{nameof(CreateZipArchiveStep)}: invalid file name {m_archiveFileName}!");
+            }
+
+            // var dir = Path.Combine(Environment.CurrentDirectory, m_directory.ToString());
+            //
+            // if (!Directory.Exists(dir))
+            // {
+            //     throw new Exception($"{nameof(CreateZipArchiveStep)}: failed due to directory {dir} doesn't exist!");
+            // }
+            //
+            //
+
+            if (!Directory.Exists(m_directory.ToString()))
+            {
+                throw new Exception($"{nameof(CreateZipArchiveStep)}: failed due to directory {m_directory} doesn't exist!");
+            }
+
+            
+            
             
             // Debug.Log(Assembly.GetExecutingAssembly().De);
             // Debug.Log(Environment.CurrentDirectory);
@@ -53,12 +80,7 @@ namespace UniTools.IO
             //
             // return;
             //
-            
-            
-            
-            
-            
-            
+
             // tar -czvf archive.zip *   
             Tar tar = Cli.Tool<Tar>();
             if (tar == null || !tar.IsInstalled)
