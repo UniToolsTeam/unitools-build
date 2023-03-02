be717fa372e8e2189b52d849b3ba8f6550bffa79
Stanislav Felinskyi <stanisalv.felinskyi@innovecs.com>
2023-03-02 16:19:37 +0200

    Misc: Updates

--- Editor/BatchMode/BatchModeBuilder.cs
+++ Editor/BatchMode/BatchModeBuilder.cs
@@ -1,18 +1,95 @@
 using System;
-using System.Linq;
 using System.Threading.Tasks;
 using UnityEditor;
-using UnityEditor.Build.Reporting;
 using UnityEngine;
 
 namespace UniTools.Build
 {
     public static class BatchModeBuilder
     {
-        
         //TODO Batchmode temporary disabled
-        
-        
+
+        public static async void Execute()
+        {
+            if (!Application.isBatchMode)
+            {
+                //TODO Implement BatchMode exception
+                throw new Exception($"{nameof(BatchModeBuilder)}: can be run only from the BatchMode!");
+            }
+
+            BatchModeParameters parameters = CommandLineParser.Parse<BatchModeParameters>(Environment.CommandLine);
+
+            string pipelineName = parameters.Pipeline;
+
+            if (Find(pipelineName) != null)
+            {
+                await RunBuildPipeline(pipelineName);
+            }
+            else
+            {
+                throw new Exception($"Failed to find a pipeline with name {pipelineName}!");
+            }
+        }
+
+        private static BuildPipeline Find(string pipelineName)
+        {
+            string[] guids = AssetDatabase.FindAssets($"t:{nameof(BuildPipeline)}");
+
+            foreach (string guid in guids)
+            {
+                string path = AssetDatabase.GUIDToAssetPath(guid);
+                BuildPipeline pipeline = AssetDatabase.LoadAssetAtPath<BuildPipeline>(path);
+                if (pipeline == null)
+                {
+                    continue;
+                }
+
+                if (pipeline.name.Equals(pipelineName))
+                {
+                    return pipeline;
+                }
+            }
+
+            return null;
+        }
+
+        private static async Task RunBuildPipeline(string name)
+        {
+            BuildPipeline pipeline = Find(name);
+            if (pipeline == null)
+            {
+                throw new Exception($"Failed to find a pipeline with name {name}!");
+            }
+
+            int stepsCount = pipeline.Count;
+            for (int i = 0; i < stepsCount; i++)
+            {
+                await pipeline.RunStep(i);
+                pipeline = Find(name);
+                if (pipeline == null)
+                {
+                    throw new Exception($"Failed to find a pipeline with name {name}!");
+                }
+            }
+
+            // await pipeline.PreBuild();
+            // BuildReport report = await pipeline.Build();
+            // BuildSummary summary = report.summary;
+            // if (summary.result == BuildResult.Failed)
+            // {
+            //     throw new Exception($"{nameof(BatchModeBuilder)}: {name} failed!");
+            // }
+            //
+            // //after the build we have to find a pipeline again due to unity clean all serialize fields and previous reference is invalid
+            // pipeline = Find<BuildPipeline>(name);
+            // if (pipeline == null)
+            // {
+            //     throw new Exception($"Failed to find a pipeline with name {name}!");
+            // }
+            //
+            // await pipeline.PostBuild();
+        }
+
         // public static async void Execute()
         // {
         //     if (!Application.isBatchMode)
--- Editor/Core/Pipelines/BuildPipeline.cs
+++ Editor/Core/Pipelines/BuildPipeline.cs
@@ -21,6 +21,43 @@ namespace UniTools.Build
 
         [SerializeField] private PipelineStep[] m_steps = default;
 
+        public int Count => m_steps.Length;
+
+        public async Task RunStep(int index)
+        {
+            if (index < 0 || index >= Count)
+            {
+                throw new Exception($"{nameof(BuildPipeline)}: Invalid step index {index}!");
+            }
+
+            try
+            {
+                EditorUtility.ClearProgressBar();
+                if (m_steps[index] == null || m_steps[index].Step == null)
+                {
+                    Debug.LogWarning($"{nameof(ScriptablePostBuildPipeline)}: The step at index {index} is null!");
+
+                    return;
+                }
+
+                EditorUtility.DisplayProgressBar($"Execute {m_steps[index].Step.name} ({index + 1}/{m_steps.Length})...", $"{m_steps[index].Step.name} ", index / (float)Count);
+
+                if (m_steps[index].Skip)
+                {
+                    Debug.LogWarning($"{nameof(ScriptablePostBuildPipeline)}: The {m_steps[index].Step.name} was skipped!");
+
+                    return;
+                }
+
+                await m_steps[index].Step.Execute();
+                EditorUtility.ClearProgressBar();
+            }
+            finally
+            {
+                EditorUtility.ClearProgressBar();
+            }
+        }
+
         [ContextMenu("Run")]
         // public override async Task Run()
         public async Task Run()
@@ -39,32 +76,46 @@ namespace UniTools.Build
 
             try
             {
-                for (int i = 0; i < m_steps.Length; i++)
+                for (int i = 0; i < Count; i++)
                 {
-                    EditorUtility.ClearProgressBar();
-                    if (m_steps[i] == null || m_steps[i].Step == null)
-                    {
-                        Debug.LogWarning($"{nameof(ScriptablePostBuildPipeline)}: The step at index {i} is null!");
-
-                        continue;
-                    }
-
-                    EditorUtility.DisplayProgressBar($"Execute {m_steps[i].Step.name} ({i + 1}/{m_steps.Length})...", $"{m_steps[i].Step.name} ", i / (float)m_steps.Length);
-
-                    if (m_steps[i].Skip)
-                    {
-                        Debug.LogWarning($"{nameof(ScriptablePostBuildPipeline)}: The {m_steps[i].Step.name} was skipped!");
-
-                        continue;
-                    }
-
-                    await m_steps[i].Step.Execute();
+                    await RunStep(i);
                 }
             }
-            finally
+            catch (Exception e)
             {
-                EditorUtility.ClearProgressBar();
+                Debug.LogException(e);
+
+                throw e;
             }
+
+            // try
+            // {
+            //     for (int i = 0; i < m_steps.Length; i++)
+            //     {
+            //         EditorUtility.ClearProgressBar();
+            //         if (m_steps[i] == null || m_steps[i].Step == null)
+            //         {
+            //             Debug.LogWarning($"{nameof(ScriptablePostBuildPipeline)}: The step at index {i} is null!");
+            //
+            //             continue;
+            //         }
+            //
+            //         EditorUtility.DisplayProgressBar($"Execute {m_steps[i].Step.name} ({i + 1}/{m_steps.Length})...", $"{m_steps[i].Step.name} ", i / (float)m_steps.Length);
+            //
+            //         if (m_steps[i].Skip)
+            //         {
+            //             Debug.LogWarning($"{nameof(ScriptablePostBuildPipeline)}: The {m_steps[i].Step.name} was skipped!");
+            //
+            //             continue;
+            //         }
+            //
+            //         await m_steps[i].Step.Execute();
+            //     }
+            // }
+            // finally
+            // {
+            //     EditorUtility.ClearProgressBar();
+            // }
 
             // try
             // {
